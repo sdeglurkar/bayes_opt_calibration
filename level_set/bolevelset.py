d627cea4371627a2aa96d53b96804f19eb9e2daf
Sampada Deglurkar <sampada_deglurkar@berkeley.edu>
2025-10-15 15:58:15 -0700

    Basic framework for fitting GP to samples

---
+++ level_set/bolevelset.py
@@ -0,0 +1,126 @@
+import GPy
+import numpy as np
+from scipy.stats import norm
+# from IPython.display import display
+import matplotlib.pyplot as plt
+import dill
+
+class BOLevelSet:
+    def __init__(self, f, init_fn, input_dim, candidates, range_x, noise_var, cost_thres, conf_thres, length_scale, logdir=None):
+        self.f = f
+        self.init_fn = init_fn
+        self.input_dim = input_dim
+        self.candidates = candidates
+        self.range_x = range_x
+        self.noise_var = noise_var
+        self.length_scale = length_scale
+        # DO NOT instantiate kernel since it has issue pickling
+        # self.kernel = GPy.kern.Matern52(input_dim, variance=1., ARD=False)
+        # self.kernel = GPy.kern.Matern52(input_dim, lengthscale=length_scale, ARD=False)
+        #self.kernel = GPy.kern.RBF(input_dim,lengthscale=0.5)
+        self.cost_thres = cost_thres
+        self.conf_thres = conf_thres
+        self.logdir = logdir
+
+    def initial_setup(self, warmstart_sample):
+        X_init = []
+        for i in range(self.input_dim):
+            X_i_init = np.random.uniform(
+                self.range_x[i][0],
+                self.range_x[i][1],
+                (warmstart_sample,)
+            )
+            X_init.append(X_i_init)
+        self.X = np.stack(X_init, axis=1)
+        self.Y = self.f(self.X) #+ np.random.randn(warmstart_sample,1)*np.sqrt(self.noise_var)
+        # self.m = GPy.models.GPRegression(
+        #     self.X, 
+        #     self.Y, 
+        #     GPy.kern.Matern52(self.input_dim, lengthscale=self.length_scale, ARD=False)
+        #     )
+        self.m = GPy.models.GPRegression(
+            self.X, 
+            self.Y, 
+            GPy.kern.Matern52(self.input_dim, lengthscale=self.length_scale, ARD=False),
+            noise_var = self.noise_var,
+            mean_function=self.init_fn
+            )
+        self.m.optimize(messages=True)
+        self.acq = self.MILE(self.candidates, self.cost_thres, self.conf_thres)
+        # print("self.acq", self.acq)
+        self.plot()
+        self.save(self.logdir + f'/bols_init')
+
+    def MILE(self, x, cost_thres, conf_thres):
+        beta = norm.ppf(conf_thres)
+        objecs = np.zeros((len(x), 1))
+        mu, cov = self.m.predict(x, full_cov=True, include_likelihood=True)
+        var = np.diag(cov)
+        # print("cov", cov) # checking for order of magnitude of covariances
+        # print("var", var)
+        for i in range(x.shape[0]):
+            var_gpp = var - (cov[:, i]) ** 2 / (var[i] + self.noise_var)
+            # print("subzeros in var_gpp", np.sum(var_gpp < 0))
+            zvec = np.sqrt(var[i] + self.noise_var) / np.abs(cov[:, i]) * (cost_thres - mu - beta * np.sqrt(var_gpp))
+            objecs[i] = np.sum(norm.cdf(zvec))
+        return objecs
+
+    @staticmethod
+    def normalize(data, scale=1):
+        return (data-np.min(data))/(np.max(data)-np.min(data)) * scale
+
+    def optimize_once(self, plot=True, save=False, iter=0):
+        max_acq_idx = np.unravel_index(np.argmax(self.acq, axis=None), self.acq.shape)
+        x_next = self.candidates[max_acq_idx[0], :][np.newaxis, :]
+        y_next = self.f(x_next) #+ np.random.randn(1,1)*np.sqrt(self.noise_var)
+        self.X = np.vstack((self.X, x_next))
+        self.Y = np.vstack((self.Y, y_next))
+        print("self.Y", self.Y)
+        self.m.set_XY(X=self.X, Y=self.Y)
+        self.m.optimize(messages=True)
+        self.acq = self.MILE(self.candidates, self.cost_thres, self.conf_thres)
+        if plot:
+            self.plot(iter=iter)
+        if save:
+            self.save(self.logdir + f'/bols_{iter}')
+        
+    def optimize_loop(self, iters, plot_every=5, save_every=5):
+        for i in range(iters):
+            print(f"optimizing step {i}")
+            self.optimize_once(plot=((i+1) % plot_every == 0), save=((i+1) % save_every == 0), iter=i)
+
+    def extract_levelset(self, x_test):
+        beta = norm.ppf(self.conf_thres)
+        mu, var = self.m.predict(x_test, full_cov=False)
+        criterion = mu + beta * np.sqrt(var)
+        return x_test[(criterion < self.cost_thres).squeeze()]
+
+    def plot(self, iter=0):
+        if self.input_dim == 1:
+            self.m.plot(plot_limits=np.array([self.range_x[0]-0.25, self.range_x[1]+0.25]))
+            plt.plot(self.candidates.flatten(), BOLevelSet.normalize(self.acq.flatten(), 10), c='orange')
+        if self.input_dim == 2:
+            self.m.plot()
+            plt.savefig(self.logdir + f'/gp_{iter}.png')
+            plt.figure()
+            x = self.candidates[:, 0].flatten()
+            y = self.candidates[:, 1].flatten()
+            original_cand_len = int(np.sqrt(len(x)))
+            original_x = x.reshape((original_cand_len, original_cand_len))[0, :]
+            original_y = y.reshape((original_cand_len, original_cand_len))[:, 0]
+            z = self.acq.reshape((original_cand_len, original_cand_len))
+            plt.contourf(original_x, original_y, BOLevelSet.normalize(z))
+            plt.colorbar()
+            plt.savefig(self.logdir + f'/mile_{iter}.png')
+
+    def save(self, fname):
+        temp_f = self.f
+        self.f = None
+        with open(fname, 'wb') as f:
+            dill.dump(self, f)
+        self.f = temp_f
+
+    @classmethod
+    def load(cls, fname):
+        with open(fname, 'rb') as f:
+            return dill.load(f)
--- level_set/level_set_estimation.py
+++ level_set/level_set_estimation.py
@@ -1,30 +1,147 @@
+# export PYTHONPATH=/Users/sampada/Documents/Research/Bayesian_Optimization/code/bayes_opt_calibration:$PYTHONPATH
+
+import GPy
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import torch
 
+from bolevelset import BOLevelSet
 from dynamics import dynamics
+from scipy.io import loadmat
 
+# mat_contents = loadmat('runs/dubins3dexpertavoidtube/ground_truth.mat')
+# print(mat_contents['data'].shape)
+# # print(mat_contents['g'].shape)
+# print(mat_contents['tau'])
+# mat_contents = loadmat('runs/dubins3dexpertreachtube/ground_truth.mat')
+# print(mat_contents['data'].shape)
+# # print(mat_contents['g'].shape)
+# print(mat_contents['tau'])
 
-dt = 0.0001
+dt = 0.001
 
 
 goalR = 1.0
 velocity = 1.0
 omega_max = 1.0
 angle_alpha_factor = 1.0
-set_mode = 'reach'
+set_mode = 'avoid'
 diff_model = True  # Doesn't matter
-freeze_model = False # Doesn't matter
+freeze_model = False # Doesn't matter - to avoid a NotImplementedError
 system = dynamics.Dubins3D(goalR, velocity, omega_max, angle_alpha_factor, set_mode, diff_model, freeze_model)
 
-print("\n")
-ctrl = system.optimal_control([0.0, 0.0, 0.0], torch.Tensor([1.0, 1.0, 1.0]))
-print(ctrl)
-print(system.dsdt(torch.Tensor([0.0, 0.0, 0.0]), ctrl, disturbance=None))
+# print("\n")
+# state = torch.Tensor([-1.0, 0.5, 0.0])
+# dvds = torch.Tensor([1.0, 1.0, 1.0])
+# ctrl = system.optimal_control(state, dvds)
+# print(ctrl)
+# dsdt = system.dsdt(state, ctrl, disturbance=None)
+# print(dsdt)
+
+# state_traj = [state]
+# curr_state = state
+# time_steps = 1000
+# for t in range(time_steps):
+#     # dvds = torch.Tensor([1.0, 1.0, 1.0])
+#     ctrl = system.optimal_control(curr_state, dvds)
+#     dsdt = system.dsdt(curr_state, ctrl, disturbance=None)
+#     next_state = system.equivalent_wrapped_state(curr_state + dt*dsdt)
+#     state_traj.append(next_state)
+#     curr_state = next_state
+# state_traj = torch.Tensor(np.array(state_traj))
+
+# fig, ax = plt.subplots()
+# circle = plt.Circle((0, 0), goalR, color='r', fill=False)
+# ax.scatter(state_traj[:, 0], state_traj[:, 1])
+# ax.add_patch(circle)
+# ax.set_aspect('equal', adjustable='box')
+# plt.show()
+
+# print(system.cost_fn(state_traj))
 
 
-# system.equivalent_wrapped_state(state_trajs[:, k] + dt*system.dsdt(state_trajs[:, k], ctrl_trajs[:, k], None))
+def get_dvds(value_fn, state, delta=0.01):
+    state = np.array([state[:2]])
+    grad = []
+    for i in range(len(state[0])):
+        direction = np.zeros(len(state[0]))
+        direction[i] = 1.0
+        delta_state = state + direction*delta
+        partial_deriv = (value_fn(delta_state) - value_fn(state))/delta 
+        grad.append(partial_deriv)
+    # For theta
+    grad.append(0.0)
+    return torch.Tensor(grad)
+
+def batched_rollouts_generator(value_fn, system=system, theta=0.0, time_steps=10000, dt=dt):
+    def rollout(state, plot_traj=True):
+        state = list(state)
+        state.append(theta)
+        state = torch.Tensor(state) 
+        state_traj = [state]
+        curr_state = state
+        for t in range(time_steps):
+            # dvds = torch.Tensor([1.0, 1.0, 1.0])
+            dvds = get_dvds(value_fn, state)
+            ctrl = system.optimal_control(curr_state, dvds)
+            dsdt = system.dsdt(curr_state, ctrl, disturbance=None)
+            next_state = system.equivalent_wrapped_state(curr_state + dt*dsdt)
+            state_traj.append(next_state)
+            curr_state = next_state
+        state_traj = torch.Tensor(np.array(state_traj))
+
+        if plot_traj:
+            fig, ax = plt.subplots()
+            circle = plt.Circle((0, 0), goalR, color='r', fill=False)
+            ax.scatter(state_traj[:, 0], state_traj[:, 1])
+            ax.add_patch(circle)
+            ax.set_aspect('equal', adjustable='box')
+            plt.show()
+
+        cost = system.cost_fn(state_traj)
+        print(state, cost)
+        return cost
+    
+    def batched_rollouts(states):
+        costs = []
+        for state in states:
+            cost = np.array(rollout(state))
+            costs.append(cost)
+        print("Done running rollouts!")
+        return np.expand_dims(np.array(costs), -1)
+    
+    return batched_rollouts
+
+
+mean_function = GPy.core.Mapping(2,1)
+mean_function.f = lambda x: x[0][0]**2 + x[0][1]**2 - 1
+mean_function.update_gradients = lambda a,b: 0
+mean_function.gradients_X = lambda a,b: 0
+value_fn = mean_function.f
+
+f = batched_rollouts_generator(value_fn) 
+input_dim = 2
+oned_x = np.arange(-5, 5, 1)
+xv, yv = np.meshgrid(oned_x, oned_x)
+candidates = np.hstack((xv.reshape(-1, 1), yv.reshape(-1, 1)))
+range_x = [[-5, 5], [-5, 5]]
+noise_var = 1e-15
+cost_thres = 0.0 
+conf_thres = 0.9
+length_scale = 0.25
+logdir = 'model_dir'
+bo_init_iters = 10
+bols = BOLevelSet(f, mean_function, input_dim, candidates, range_x, noise_var, cost_thres, conf_thres, length_scale, logdir)
+bols.initial_setup(bo_init_iters)
+print("\nCompleted BOLevelSet initial setup")
+# bols.optimize_loop(bo_iters)
+
+print(bols.extract_levelset(candidates))
+
+mu, cov = bols.m.predict(candidates, full_cov=True, include_likelihood=True)
+value_fn = mu
+
 
 
 print("\nYAY!")
