f21e15dda16f8e9e5129c1e0d9883e84acb55e97
Sampada Deglurkar <sampada_deglurkar@berkeley.edu>
2025-11-12 13:55:43 -0800

    Fitting GP using the HJ optimal controller

--- level_set/hj_toolbox_experiments_v2.py
+++ level_set/hj_toolbox_experiments_v2.py
@@ -1,19 +1,21 @@
+import GPy
+import hj_reachability as hj
 import jax
 import jax.numpy as jnp
-import numpy as np
-
-from IPython.display import HTML
-import matplotlib.animation as anim
 import matplotlib.pyplot as plt
+import numpy as np
 import plotly.graph_objects as go
 
-import hj_reachability as hj
-
+from bolevelset import BOLevelSet
 
 THETA_INDEX = 0 #30
 THETA_VALUE = 0.0
 DUBINS_VELOCITY = 10.0
 DT = 0.01
+FINAL_TIME = -1.0
+TRAJ_TIME_STEPS = int(np.abs(FINAL_TIME)/DT)
+goal_R = 5
+NUM_BO_INIT_ITERS = 100
 
 class DubinsCar(hj.ControlAndDisturbanceAffineDynamics):
 
@@ -81,10 +83,133 @@ grid = hj.Grid.from_lattice_parameters_and_boundary_conditions(hj.sets.Box(np.ar
                                                                            np.array([15., 15., 2 * np.pi])),
                                                                (51, 40, 50),
                                                                periodic_dims=2)
-values = jnp.linalg.norm(grid.states[..., :2], axis=-1) - 5
+
+
+######### GET OPTIMAL VALUE FUNCTION -- BRT #########
+values = jnp.linalg.norm(grid.states[..., :2], axis=-1) - goal_R
 
 solver_settings = hj.SolverSettings.with_accuracy("very_high",
                                                   hamiltonian_postprocessor=hj.solver.backwards_reachable_tube)
+
+
+times = np.linspace(0, FINAL_TIME, 100) 
+initial_values = values
+all_values = hj.solve(solver_settings, dynamics, grid, times, initial_values)
+
+######### FIT GAUSSIAN PROCESS #########
+mean_function = GPy.core.Mapping(2,1)
+mean_function.f = lambda x: np.expand_dims(x[:, 0]**2 + x[:, 1]**2 - goal_R, -1)
+mean_function.update_gradients = lambda a,b: 0
+mean_function.gradients_X = lambda a,b: 0
+
+def batched_rollouts_generator(value_fn, time_steps=TRAJ_TIME_STEPS, dt=DT):
+    def rollout(state, plot_traj=False):
+        orig_state = state
+        state_traj = [state]
+        for _ in range(time_steps):
+            index = grid.nearest_index(state)
+            grad_val = grid.grad_values(value_fn[-1, :, :, :])[index[0], index[1], index[2]]
+            control = dynamics.optimal_control(None, None, grad_val)
+            state = state + DT*dynamics(state, control, disturbance=np.array([0.]), time=None)
+            state_traj.append(state)
+        state_traj = np.array(state_traj)
+
+        cost = jnp.min(jnp.linalg.norm(state_traj, axis=-1) - goal_R, axis=-1)
+        print("Ran one rollout", orig_state, cost)
+        return cost
+    
+    def batched_rollouts(states):
+        costs = []
+        for state in states:
+            # if SYSTEM_TYPE == 'DOUBLE_INT':
+            #     state = np.append(state, 1.0)
+            state = np.append(state, THETA_VALUE)
+            cost = np.array(rollout(state))
+            costs.append(cost)
+        print("Done running rollouts!")
+        return np.expand_dims(np.array(costs), -1)
+    
+    return batched_rollouts
+
+f = batched_rollouts_generator(all_values)
+input_dim = 2
+oned_x = np.arange(-15, 15, 1.0)
+xv, yv = np.meshgrid(oned_x, oned_x)
+candidates = np.hstack((xv.reshape(-1, 1), yv.reshape(-1, 1)))
+range_x = [[-11, 6], [-6, 6]]
+noise_var = 0.001 
+cost_thres = 0.0 
+conf_thres = 0.9
+length_scale = 0.25
+logdir = 'dubins_hj_model_dir'
+bo_init_iters = NUM_BO_INIT_ITERS
+bols = BOLevelSet(f, mean_function, input_dim, candidates, range_x, noise_var, cost_thres, conf_thres, length_scale, logdir)
+bols.initial_setup(bo_init_iters)
+print("\nCompleted BOLevelSet initial setup")
+# bo_iters = 150
+# bols.optimize_loop(bo_iters)
+level_set = bols.extract_levelset(candidates)
+print("Level Set\n", level_set)
+# plt.figure()
+# plt.scatter([elem[0] for elem in level_set], [0*elem[0] for elem in level_set])
+# plt.savefig(logdir + f'/level_set.png')
+
+
+
+######### PLOTTING - BRT #########
+if False:
+    vmin, vmax = all_values.min(), all_values.max()
+    levels = np.linspace(round(vmin), round(vmax), round(vmax) - round(vmin) + 1)
+
+    def render_frame(i, colorbar=False, traj=None):
+        fig = plt.figure(figsize=(13, 8))
+        plt.contourf(grid.coordinate_vectors[0],
+                    grid.coordinate_vectors[1],
+                    all_values[i, :, :, THETA_INDEX].T,
+                    vmin=vmin,
+                    vmax=vmax,
+                    levels=levels)
+        if colorbar:
+            plt.colorbar()
+        # plt.contour(grid.coordinate_vectors[0],
+        #             grid.coordinate_vectors[1],
+        #             target_values[:, :, THETA_INDEX].T,
+        #             levels=0,
+        #             colors="black",
+        #             linewidths=3)
+        plt.contour(grid.coordinate_vectors[0],
+                    grid.coordinate_vectors[1],
+                    all_values[0, :, :, THETA_INDEX].T,
+                    levels=0,
+                    colors="black",
+                    linewidths=3)
+        plt.contour(grid.coordinate_vectors[0],
+                    grid.coordinate_vectors[1],
+                    all_values[i, :, :, THETA_INDEX].T,
+                    levels=0,
+                    colors="black",
+                    linewidths=3)
+        
+        if traj is not None:
+            plt.scatter(traj[:, 0], traj[:, 1], c='red')
+
+
+    render_frame(-1, True)
+    render_frame(70, True)
+    render_frame(50, True)
+    render_frame(20, True)
+    render_frame(10, True)
+    render_frame(0, True)
+    plt.show()
+
+
+
+
+
+
+
+
+
 
 
 # time = 0.
@@ -112,73 +237,6 @@ solver_settings = hj.SolverSettings.with_accuracy("very_high",
 #                              surface_count=1,
 #                              isomax=0))
 # fig.show()
-
-
-times = np.linspace(0, -1.0, 100) 
-initial_values = values
-all_values = hj.solve(solver_settings, dynamics, grid, times, initial_values)
-
-vmin, vmax = all_values.min(), all_values.max()
-levels = np.linspace(round(vmin), round(vmax), round(vmax) - round(vmin) + 1)
-
-start_state = np.array([-10, 0, THETA_VALUE])
-state = start_state
-all_states = [state]
-print("Generating trajectory from state", start_state)
-for _ in range(200):
-  index = grid.nearest_index(start_state)
-  grad_val = grid.grad_values(all_values[-1, :, :, :])[index[0], index[1], index[2]]
-  control = dynamics.optimal_control(None, None, grad_val)
-  state = state + DT*dynamics(state, control, disturbance=np.array([0.]), time=None)
-  all_states.append(state)
-all_states = np.array(all_states)
-print("Done")
-
-
-def render_frame(i, colorbar=False, traj=None):
-    fig = plt.figure(figsize=(13, 8))
-    plt.contourf(grid.coordinate_vectors[0],
-                 grid.coordinate_vectors[1],
-                 all_values[i, :, :, THETA_INDEX].T,
-                 vmin=vmin,
-                 vmax=vmax,
-                 levels=levels)
-    if colorbar:
-        plt.colorbar()
-    # plt.contour(grid.coordinate_vectors[0],
-    #             grid.coordinate_vectors[1],
-    #             target_values[:, :, THETA_INDEX].T,
-    #             levels=0,
-    #             colors="black",
-    #             linewidths=3)
-    plt.contour(grid.coordinate_vectors[0],
-                grid.coordinate_vectors[1],
-                all_values[0, :, :, THETA_INDEX].T,
-                levels=0,
-                colors="black",
-                linewidths=3)
-    plt.contour(grid.coordinate_vectors[0],
-                grid.coordinate_vectors[1],
-                all_values[i, :, :, THETA_INDEX].T,
-                levels=0,
-                colors="black",
-                linewidths=3)
-    
-    if traj is not None:
-      plt.scatter(traj[:, 0], traj[:, 1], c='red')
-
-
-render_frame(-1, True, all_states)
-render_frame(70, True)
-render_frame(50, True)
-render_frame(20, True)
-render_frame(10, True)
-render_frame(0, True)
-plt.show()
-
-
-
-
 
 
 
