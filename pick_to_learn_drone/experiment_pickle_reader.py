5f9ec1dcd7bf09722024ebedc8f0651f2b0887ed
Sampada Deglurkar <sampada_deglurkar@berkeley.edu>
2026-02-23 17:55:10 -0800

    More restructuring

--- pick_to_learn_drone/experiment_pickle_reader.py
+++ pick_to_learn_drone/experiment_pickle_reader.py
@@ -9,3 +9,6 @@ with open(f"{EXPERIMENT_PICKLE_NAME}.pkl", "rb") as f:
     results = pickle.load(f)
 
 print(results.keys())
+print(results['our_method'].keys())
+print(results['albert_itr_method'].keys())
+print(results['albert_robust_method'].keys())
