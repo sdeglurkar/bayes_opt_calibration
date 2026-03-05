b54a7a8b3399249ce26ed5fb256feaa3afded3bb
Sampada Deglurkar <sampada_deglurkar@berkeley.edu>
2026-03-04 18:34:36 -0800

    Essential experiments for 2D and 3D

--- pick_to_learn_drone/pick_to_learn_settings.py
+++ pick_to_learn_drone/pick_to_learn_settings.py
@@ -12,11 +12,11 @@ from scipy.stats import norm
 
 ########################### DYNAMICS SETTINGS ###########################
 # Basic slice
-ADVERSARY_SETTING = [0.4, 0.0, -2.2, 0.3, 0.0, 0.0]  # ad_x, ad_vx, ad_y, ad_vy, ad_z, ad_vz
-EGO_SETTING = [0.0, 0.7, 0.0, 0.0]  # ego_vx, ego_vy, ego_z, ego_vz
-# New slice 1
 # ADVERSARY_SETTING = [0.4, 0.0, -2.2, 0.3, 0.0, 0.0]  # ad_x, ad_vx, ad_y, ad_vy, ad_z, ad_vz
-# EGO_SETTING = [0.0, 0.0, 0.05, -0.5]  # ego_vx, ego_vy, ego_z, ego_vz
+# EGO_SETTING = [0.0, 0.7, 0.0, 0.0]  # ego_vx, ego_vy, ego_z, ego_vz
+# New slice 1
+ADVERSARY_SETTING = [0.4, 0.0, -2.2, 0.3, 0.0, 0.0]  # ad_x, ad_vx, ad_y, ad_vy, ad_z, ad_vz
+EGO_SETTING = [0.0, 0.0, 0.05, -0.5]  # ego_vx, ego_vy, ego_z, ego_vz
 
 # This is the environment boundary
 # RANGE_X = [[-1.0, 1.0], [-1.0, 1.0], [-3.2, 0.0], [0.1, 1.0], [-1.0, 1.0], [-1.0, 1.0],
@@ -57,8 +57,8 @@ elif INPUT_DIM == 6:
 DELTA = 1e-4
 
 ########################### ACQUISITION FN SETTINGS ###########################
-ALPHA = 0.05
-TOLERANCE_ALPHA = 0.03
+ALPHA = 0.1
+TOLERANCE_ALPHA = 0.05
 RANDOM_ACQUISITION = True
 
 DECAY_RATE = 0.95
@@ -83,7 +83,7 @@ ROBUST_ALBERT_ALPHA_SWEEP = [0.0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.75, 0.9, 1.0
 
 ########################### OTHER SETTINGS ###########################
 EXPERIMENT_STRING = str(INPUT_DIM) + \
-    'D_basicslice_randomacq_N4000_init40_decay0.95thres0.3_alpha0.05_tolalpha0.03'
+    'D_newslice1_randomacq_N4000_init40_decay0.95thres0.3_alpha0.1_tolalpha0.05'
 
 # VALIDATION_DISCRETIZATION = [0.05, 0.01, 0.05, 0.01, 0.01, 0.01, \
 #                             0.05, 0.01, 0.05, 0.01, 0.01, 0.01]
